1acfc2e37d6c23ed0d21ed2be75ce2452ecf780d
Julian Pasquale <julianagustin78@gmail.com>
2017-12-12 20:04:01 -0300

    modelos agregados y comienzo con las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  resources :grades
+  resources :evaluations
+  resources :students
+  resources :courses
+  root to: 'courses#index'
   devise_for :users, only: [:sessions]
+
+  resources :courses do
+  	resources :evaluations
+  	resources :students
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211181155) do
+ActiveRecord::Schema.define(version: 20171212221116) do
+
+  create_table "courses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "year", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "evaluations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "tittle"
+    t.float "min_grade", limit: 24
+    t.date "date"
+    t.bigint "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_evaluations_on_course_id"
+  end
+
+  create_table "grades", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.float "grade", limit: 24, default: -1.0, null: false
+    t.bigint "student_id"
+    t.bigint "evaluation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["evaluation_id"], name: "index_grades_on_evaluation_id"
+    t.index ["student_id"], name: "index_grades_on_student_id"
+  end
+
+  create_table "students", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "lastname", null: false
+    t.integer "dni", null: false
+    t.string "number", limit: 50, null: false
+    t.bigint "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_students_on_course_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -29,4 +67,8 @@ ActiveRecord::Schema.define(version: 20171211181155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "evaluations", "courses"
+  add_foreign_key "grades", "evaluations"
+  add_foreign_key "grades", "students"
+  add_foreign_key "students", "courses"
 end
