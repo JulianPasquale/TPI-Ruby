bd31a83541cfe4f4dd8fec9c3c357491cb722c5b
julian <julianagustin78@gmail.com>
2017-12-20 20:38:12 -0300

    comienzo con los tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,29 @@
-require 'test_helper'
+#require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create course" do
+  	c=Course.new(name:'course_test', year:2017)
+    assert_equal(true,c.save)
+    c.destroy
+  end
+
+  test "should not create course" do
+  	c1=Course.new(name:'Course_test', year:2017).save
+    c2=Course.new(name:'Course_test', year:2017)
+    assert_equal(false,c2.save)
+    c1.destroy
+    c2.destroy
+  end
+
+  test "should delete dependents" do
+    c= Course.create(name:'Course_evaluation', year:2020)
+    e=c.evaluations.create!(id:101,tittle:'testing', min_grade:4, date:Date.today)
+    assert_equal(true, e.save)
+    assert_equal(true, Evaluation.find(101).empty?)
+    c.destroy
+    assert_equal(Course.last,c)
+    e.destroy
+  end
+  
+
 end
