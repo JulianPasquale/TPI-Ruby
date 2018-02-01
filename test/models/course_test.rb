3c7be250437b4ca6f5c80d1ca93077086a344bef
Julian Pasquale <julian.pasquale@snappler.com>
2018-02-01 20:04:41 -0300

    arreglando tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,32 +1,52 @@
-#require 'test_helper'
+require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  test "should create course" do
-  	c=Course.new(name:'course_test', year:2017)
-    assert(true,c.save)
-    c.destroy
+  
+  def setup
+    @course = courses(:curso_uno)
+  end
+
+  test "Should create course" do
+    assert_difference('Course.count') do
+      Course.create(name: (@course.name + '2'), year: @course.year)
+    end
   end
 
   test "should not create course" do
-  	c1=Course.find_or_create_by(name:'Course_test1', year:Date.today.year)
-    #Already exists
-    c2=Course.new(name:'Course_test1', year:Date.today.year)
-    #Year must be greater
-    c3=Course.new(name:'Course_test1', year:Date.today.year-10)
-    refute(c2.save)
-    refute(c3.save)
-    c1.destroy
+    assert_difference('Course.count', 0) do
+      Course.create(name:@course.name, year:@course.year)
+    end
+    assert_difference('Course.count', 0) do
+      Course.create(name:'Course_test1', year:@course.year-10)
+    end
   end
 
   test "should delete dependents" do
-     @course= Course.find_or_create_by(name:'Course_evaluations_model_test', year:Date.today.year)
-     @evaluation = @course.evaluations.create(tittle:'Evaluation_year', min_grade:5, date:(Date.today))
-     assert_equal(@evaluation, Evaluation.find(@evaluation.id))
+     @evaluation = @course.evaluations.create(tittle:"#{@course.name}_evaluation", min_grade:5, date:(Date.today))
+     assert(@evaluation.persisted?)
      @course.destroy
      refute(Evaluation.exists?(@evaluation.id))
   end
 
-  
+  test "name should be present" do
+    @course.name= ''
+    refute(@course.valid?)
+    assert_includes @course.errors[:name],
+      I18n.t('errors.messages.blank')
+  end
+
+  test "year should be greater" do
+    @course.year= @course.year-10
+    refute(@course.valid?)
+    assert_includes @course.errors[:year],
+      "must be greater than #{(Date.today.year) -1}"
+  end
+
+  test "should delete course" do
+    assert_difference('Course.count', -1) do
+      @course.destroy
+    end
+  end
 
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,22 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActionDispatch
+  class ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  end
+end
 
-  # Add more helper methods to be used by all tests here...
+module ActionController
+  class ActionController::TestCase
+    include Devise::Test::ControllerHelpers
+  end
+end
+
+module ActiveSupport
+  class TestCase
+    fixtures :all
+  end
 end
