1dc3ac57357ea674079f4995f5dc95b8b00c0d83
Julian Pasquale <julianagustin78@gmail.com>
2018-02-07 11:35:35 -0300

    prubas con select

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,55 +1,16 @@
 class GradesController < BackendController
-  before_action :set_evaluation
-  before_action :set_grade, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @grades = @evaluation.grades
-  end
-
-  def show; end
-
-  def new
-    @grade = Grade.new
-  end
-
-  def edit; end
-
-  def create
-    @grade = Grade.new(grade_params)
-
-    respond_to do |format|
-      if @grade.save
-        format.html { redirect_to [@evaluation.course], notice: 'Grade was successfully created.' }
-        format.json { render :show, status: :created, location: @grade }
-      else
-        format.html { render :new }
-        format.json { render json: @grade.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  before_action :set_grade, only: :update
 
   def update
+    binding.pry
     if @grade.update(grade_params)
       flash[:success] = t(:success) 
-      redirect_to [@evaluation.course]
     else
       flash[:error] = view_context.flash_error_format(@evaluation)
-      render :edit 
-    end
-  end
-
-  def destroy
-    @grade.destroy
-    respond_to do |format|
-      format.html { redirect_to [@evaluation.course], notice: 'Grade was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    def set_evaluation
-      @evaluation= Evaluation.find(params[:evaluation_id])
-    end
 
     def set_grade
       @grade = Grade.find(params[:id])
