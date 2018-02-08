cd08b958ed592b6e5766b17baca6432f7b0fe7b0
Julian Pasquale <julian.pasquale@snappler.com>
2018-02-08 19:23:36 -0300

    probando background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = true
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -57,6 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.serve_static_assets = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
