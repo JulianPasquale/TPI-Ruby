099e8c9779198f8eee73c5f028148ada1af5ce79
Julian Pasquale <julian.pasquale@snappler.com>
2018-02-08 19:38:11 -0300

    migracion para heroku

--- db/migrate/20171212221116_create_grades.rb
+++ db/migrate/20171212221116_create_grades.rb
@@ -1,7 +1,7 @@
 class CreateGrades < ActiveRecord::Migration[5.1]
   def change
     create_table :grades do |t|
-      t.float :grade, null:false, default: -1
+      t.float :grade, null:false
       t.references :student, foreign_key: true
       t.references :evaluation, foreign_key: true
 
